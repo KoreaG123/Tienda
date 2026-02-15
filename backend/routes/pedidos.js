9b45e3268a0da83a7d5e0bb4e27123f6d3e57c88
KoreaG123 <90579765+KoreaG123@users.noreply.github.com>
2026-02-14 20:22:14 -0500

    Delete backend directory

--- backend/routes/pedidos.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const pedidosController = require('../controllers/pedidosController');
-
-router.post('/', pedidosController.crearPedido);
-router.get('/', pedidosController.obtenerPedidos);
-router.put('/:id', pedidosController.actualizarPedido);
-
-module.exports = router;
